c7b69549ba2f30f3b1f83aff11337c8892fcdbe7
blu3ci <madadi.anirudh@gmail.com>
2025-06-30 20:22:06 -0500

    configured render settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import dj_database_url
 from datetime import timedelta
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-@%034gt(v19&gs7^!kvn8ya1w&_a_l88k@*e8v3_c*einnknu-"
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    default="django-insecure-@%034gt(v19&gs7^!kvn8ya1w&_a_l88k@*e8v3_c*einnknu-",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "RENDER" not in os.environ
 
 ALLOWED_HOSTS = ["*"]
 
@@ -94,10 +99,15 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
+    "default": dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default="postgresql://postgres:postgres@localhost:5432/safeststreets",
+        conn_max_age=600,
+    )
 }
 
 
